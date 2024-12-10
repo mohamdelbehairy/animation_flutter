31a85700a3394f22a065eeef8257ce3a2cf3bea6
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-10 22:11:00 +0200

    Transform

--- lib/main.dart
+++ lib/main.dart
@@ -26,73 +26,28 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-
-  @override
-  void initState() {
-    _animationController = AnimationController(
-        vsync: this,
-        lowerBound: 100,
-        upperBound: 300,
-        duration: const Duration(seconds: 3));
-    super.initState();
-  }
-
+class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Animation')),
-      body: Column(
-        children: [
-          AnimatedBuilder(
-              animation: _animationController,
-              child: Text("Mohamed",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: _animationController.value / 10, // 10 => 30
-                  )),
-              builder: (context, child) {
-                return Container(
-                  height: _animationController.value, // 100 => 300
-                  width: _animationController.value, // 100 => 300
-                  color: Colors.red,
-                  child: Center(child: child),
-                );
-              }),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    _animationController.forward();
-                  },
-                  child: const Text("Forward")),
-              ElevatedButton(
-                  onPressed: () {
-                    _animationController.reverse();
-                  },
-                  child: const Text("Reverse")),
-              ElevatedButton(
-                  onPressed: () {
-                    _animationController.stop();
-                  },
-                  child: const Text("Stop")),
-              ElevatedButton(
-                  onPressed: () {
-                    _animationController.repeat();
-                  },
-                  child: const Text("Repeat")),
-              ElevatedButton(
-                  onPressed: () {
-                    _animationController.reset();
-                  },
-                  child: const Text("Reset")),
-            ],
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(title: const Text('Animation')),
+        body: TweenAnimationBuilder(
+            tween: Tween(begin: 0.5, end: 1.5),
+            duration: const Duration(seconds: 3),
+            builder: (context, value, child) {
+              return Transform.scale(
+                scale: value,
+                origin: const Offset(100, 100),
+                child: Center(
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.red,
+                    alignment: Alignment.center,
+                    child: const Text("Container"),
+                  ),
+                ),
+              );
+            }));
   }
 }
