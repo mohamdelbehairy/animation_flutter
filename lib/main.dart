86db88fe5d5d9cd74903d75d6984ae49b1c8f0ca
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-10 22:39:26 +0200

    explicit animation with explict animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Animation());
+  runApp(const AnimationApp());
 }
 
-class Animation extends StatelessWidget {
-  const Animation({super.key});
+class AnimationApp extends StatelessWidget {
+  const AnimationApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,28 +26,43 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
+class _HomeViewState extends State<HomeView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<AlignmentGeometry> alignController;
+  late Animation<double> rotateConroller;
+
+  @override
+  void initState() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2))
+          ..repeat(reverse: true);
+    alignController = Tween<AlignmentGeometry>(
+            begin: Alignment.centerLeft, end: Alignment.centerRight)
+        .animate(animationController);
+    rotateConroller =
+        Tween<double>(begin: 0, end: 2).animate(animationController);
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Animation')),
-        body: TweenAnimationBuilder(
-            tween: Tween(begin: 0.5, end: 1.5),
-            duration: const Duration(seconds: 3),
-            builder: (context, value, child) {
-              return Transform.scale(
-                scale: value,
-                origin: const Offset(100, 100),
-                child: Center(
-                  child: Container(
-                    height: 200,
-                    width: 200,
-                    color: Colors.red,
-                    alignment: Alignment.center,
-                    child: const Text("Container"),
-                  ),
-                ),
-              );
-            }));
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AlignTransition(
+                alignment: alignController,
+                child: RotationTransition(
+                    turns: rotateConroller,
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ))),
+          ],
+        ));
   }
 }
